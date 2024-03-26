d9e6eb6b220366c4e7ee74ad7f04b1e2e3162b5e
Constantine Kim <electro.passion92@gmail.com>
2024-03-26 23:11:19 +0900

    Init

--- app/src/main/java/com/practicum/movieexample/ui/movies/MoviesActivity.kt
+++ app/src/main/java/com/practicum/movieexample/ui/movies/MoviesActivity.kt
@@ -1,6 +1,5 @@
 package com.practicum.movieexample.ui.movies
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
